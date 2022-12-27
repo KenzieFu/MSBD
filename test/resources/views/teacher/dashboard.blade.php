b11c8f368a356e4ff89da1db3a1e07742ccb6b74
LAPTOP-B0212VLT\User <kenziefubrianto@gmail.com>
2022-12-27 19:06:28 +0700

    no message

--- test/resources/views/teacher/dashboard.blade.php
+++ test/resources/views/teacher/dashboard.blade.php
@@ -2,7 +2,17 @@
 @section('teacherContent')
 <div class="m-3">Hi {{ auth()->guard('teacher')->user()->name }}</div>
 
-@if($ann)
+
+@php
+    $start = Carbon\Carbon::parse( $ann->created_at );
+$end= \Carbon\Carbon::parse( now());
+
+
+$diff_in_minutes = $end->diffInMinutes($start);
+
+@endphp
+@if($ann && $diff_in_minutes <2)
+
 <div class="max-w m-3 p-6 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
   
         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Pengumuman</h5>
